cc22384487256c1d2c77b84f76b8671cecbfa2ce
Bakirov A.R <turkin86@mail.ru>
2020-03-06 09:52:09 +0300

    начата разработка планировщика отправки сообщений

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-sender-client
 
 go 1.13
 
-require github.com/jinzhu/gorm v1.9.12 // indirect
+require (
+	github.com/carlescere/scheduler v0.0.0-20170109141437-ee74d2f83d82 // indirect
+	github.com/jinzhu/gorm v1.9.12 // indirect
+)
--- main.go
+++ main.go
@@ -2,13 +2,9 @@ package main
 import (
     "fmt"
     "os"
-    "net"
-    "io"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"bufio"
-	"strings"
-	"time"
 )
 
 type Message struct {
@@ -18,15 +14,6 @@ type Message struct {
 }
 
 func main() {
-	//Create connect to data server
-    conn, err := net.Dial("tcp", "127.0.0.1:9876")
-	//Set deadline connect
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-
-    defer conn.Close()
 	//Connect to data storage
 	db, err := gorm.Open("sqlite3", "client-msg.db")
 	if err != nil {
@@ -35,6 +22,9 @@ func main() {
 	defer db.Close()
 	// Migrate the schema
 	db.AutoMigrate(&Message{})
+	//Run tasks
+	srvAddr := "127.0.0.1:9876"
+	go TasksRun(db, srvAddr)
 	//User input message
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Message shell")
@@ -45,26 +35,6 @@ func main() {
 		text, _ := reader.ReadString('\n')
 		db.Create(&Message{Content: text, Status: 0})
 		fmt.Println("Cообщение сохранен\n")
-		//Send maessage to data server
-		conn.SetDeadline(time.Now().Add(time.Second*5))
-		resp, werr := io.CopyBuffer(conn, strings.NewReader(text), buf)
-		if resp == 0 || werr != nil {
-			fmt.Println("Не удалось отправить сообщение серверу...")
-			continue
-		} else {
-			fmt.Println("Cообщение отправлен")
-		}
-
-		//Get response from server.
-		conn.SetDeadline(time.Now().Add(time.Second*5))
-		var respBuf strings.Builder
-		resp, werr = io.CopyBuffer(&respBuf, conn, buf)
-		if resp <= 0 || werr != nil {
-			fmt.Println("Не удалось получить ответ от сервера")
-			continue
-		} else {
-			fmt.Printf("Ответ от сервера: %s", respBuf.String())
-		}
 		fmt.Print("\n")
 	}
 	fmt.Print("Работа завершена")
